63a81dc41e453b0175defacb6af25924a1d33ed7
suhail <suhailkv99@gmail.com>
2024-05-08 09:40:40 +0530

    modified the route name

--- backend/routes/vehicleRoutes.js
+++ backend/routes/vehicleRoutes.js
@@ -4,30 +4,28 @@ const Booking = require("../models/booking");
 const vehicleRouter = express.Router();
 const { Op } = require("sequelize");
 
-vehicleRouter.get("/getVehicleByType/:vehicleTypeId", async (req, res) => {
+vehicleRouter.get("/:vehicleTypeId", async (req, res) => {
   const { vehicleTypeId } = req.params;
   const vehicles = await Vehicle.findAll({
-    // Perform a left join with Bookings to check for vehicles with no active bookings
     include: [
       {
         model: Booking,
         where: {
           endDate: {
-            [Op.is]: null, // Only include bookings where endDate is null (still active)
+            [Op.is]: null,
           },
         },
-        required: false, // Left join, so we get vehicles with no bookings
+        required: false,
       },
     ],
     where: {
       vehicleTypeId: vehicleTypeId,
 
       "$Bookings.vehicleId$": {
-        [Op.is]: null, // If there's no active booking, this will be true
+        [Op.is]: null,
       },
     },
   });
   res.json(vehicles);
 });
-
 module.exports = vehicleRouter;
--- backend/routes/vehicleTypesRoutes.js
+++ backend/routes/vehicleTypesRoutes.js
@@ -2,12 +2,6 @@ const express = require("express");
 const VehicleType = require("../models/vehicleType");
 const vehicleTypeRouter = express.Router();
 
-vehicleTypeRouter.get("/", async (req, res) => {
-  const types = await VehicleType.findAll({
-    attributes: ["id", "name"],
-  });
-  res.json(types);
-});
 vehicleTypeRouter.get("/getWheelTypes", async (req, res) => {
   const wheelTypes = await VehicleType.findAll({
     attributes: ["num_of_wheels"],
@@ -16,18 +10,15 @@ vehicleTypeRouter.get("/getWheelTypes", async (req, res) => {
   res.json(wheelTypes);
 });
 
-vehicleTypeRouter.get(
-  "/VehicleTypesByWheelType/:wheelNum",
-  async (req, res) => {
-    const num_of_wheels = req.params.wheelNum;
-    const vehiclTypes = await VehicleType.findAll({
-      attributes: ["id", "name"],
+vehicleTypeRouter.get("/getWheelTypes/:wheelNum", async (req, res) => {
+  const num_of_wheels = req.params.wheelNum;
+  const vehiclTypes = await VehicleType.findAll({
+    attributes: ["id", "name"],
 
-      where: {
-        num_of_wheels,
-      },
-    });
-    res.json(vehiclTypes);
-  }
-);
+    where: {
+      num_of_wheels,
+    },
+  });
+  res.json(vehiclTypes);
+});
 module.exports = vehicleTypeRouter;
