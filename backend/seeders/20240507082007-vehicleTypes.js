98ff0036891b8c289a6b5ad6dc417ab1cc02b5ff
suhail <suhailkv99@gmail.com>
2024-05-08 09:41:06 +0530

    beutify the seeders

--- backend/seeders/20240507082007-vehicleTypes.js
+++ backend/seeders/20240507082007-vehicleTypes.js
@@ -3,15 +3,6 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-     */
     await queryInterface.bulkInsert(
       "VehicleTypes",
       [
@@ -45,12 +36,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
     await queryInterface.bulkDelete("VehicleTypes", null, {});
   },
 };
