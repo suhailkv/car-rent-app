b103fef9f315c1cd38c2f13eb62a7da4edfb31eb
suhail <suhailkv99@gmail.com>
2024-05-08 09:44:52 +0530

    added constants file

---
+++ frontend/src/constants.js
@@ -0,0 +1,8 @@
+const CONSTANTS = {
+  BACKEND_URL: "http://localhost:9090",
+  BOOKING_API: "booking",
+  VEHICLE_TYPE_API: "vehicleTypes/getWheelTypes",
+  VEHICLE_API: "vehicles",
+  GET_WHEEL_TYPE_API: "vehicleTypes/getWheelTypes",
+};
+export default CONSTANTS;
