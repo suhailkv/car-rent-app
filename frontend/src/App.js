1bc02bae75a24b2fc282b1ab42a2de46d2f2550e
suhail <suhailkv99@gmail.com>
2024-05-08 09:43:15 +0530

    added component in app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import FormComp from "./componets/formComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <FormComp />
+    </div>
+  );
+}
+
+export default App;
