383d718bda30dc439f4e40a7a35923e7eaf4d0b6
suhail <suhailkv99@gmail.com>
2024-05-08 09:39:01 +0530

    added contants file , added cors in server

---
+++ backend/constants.js
@@ -0,0 +1,6 @@
+const CONSTANTS = {
+  PORT: 9090,
+  BACKEND_URL: "http://localhost",
+  FRONTEND_URL: "http://localhost:3000",
+};
+module.exports = CONSTANTS;
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const app = express();
-
+global.CONSTANTS = require("./constants");
+var cors = require("cors");
 const vehicleTypeRouter = require("./routes/vehicleTypesRoutes");
 const vehicleRouter = require("./routes/vehicleRoutes");
 const bookingRouter = require("./routes/bookingRoutes");
 
 // middlewaeres
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: [CONSTANTS.FRONTEND_URL],
+  })
+);
 // regustering routes
 app.use("/vehicleTypes", vehicleTypeRouter);
 app.use("/vehicles", vehicleRouter);
 app.use("/booking", bookingRouter);
-app.listen(9090, () => console.log("server running"));
+app.listen(CONSTANTS.PORT, () =>
+  console.log(`server running on the port ${CONSTANTS.PORT}`)
+);
